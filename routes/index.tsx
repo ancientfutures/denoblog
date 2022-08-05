cd340c4e3b7f0c11342a57c851d34564a45ce60c
l0skepetos <loskepetos@gmail.com>
2022-08-05 13:26:26 +0700

    editing index

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,17 +5,23 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    
+    <div class={tw`flex flex-col min-h-screen`}>
+      <div class={tw`flex justify-end bg-purple-200 items-center`}>
+       <section class={tw`w-full min-h-screen flex justify-center items-center flex-col bg-purple-200`}>
+        <p class={tw`text-3xl pb-10 font-bold`}>Ancient Futures</p>
+        <a href="/" class={tw`border-1 border-black inline-flex items-center h-10 px-4 m-4 text-black bg-transparent rounded hover:bg-white`}>
+        Documentation
+       </a>
+      </section>
+      </div>
+      <footer class={tw`border-t-2 border-gray-200 bg-gray-100 h-32 flex flex-col gap-4 justify-center`}>
+        <div class={tw`text-center`}>
+           © 2022 Ancient Futures
+        </div>
+       
+      </footer>
     </div>
+    
   );
 }
